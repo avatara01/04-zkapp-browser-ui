8605418b8363d67067c8f788f9ed6d3a21ec8f68
Ketigax <ava.lenovo@gmail.com>
2022-12-14 04:20:35 +0000

    Deploy gh-pages

--- _next/static/chunks/webpack-045bed68fc8184cc.js
+++ _next/static/chunks/webpack-045bed68fc8184cc.js
@@ -121,7 +121,7 @@
 /******/ 		// This function allow to reference all chunks
 /******/ 		__webpack_require__.miniCssF = function(chunkId) {
 /******/ 			// return url for filenames based on template
-/******/ 			return "static/css/" + "b6e2afec8004b374" + ".css";
+/******/ 			return "static/css/" + "1982d198bef3e6eb" + ".css";
 /******/ 		};
 /******/ 	}();
 /******/ 	
